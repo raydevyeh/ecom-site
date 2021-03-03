903a0746ec76a0fad92e72bef31e00d3f75477e9
raydevyeh <raydevyeh@gmail.com>
2021-03-02 16:27:14 -0800

    created a route to direct to homepage when path is /

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import HomePage from './pages/homepage/homepage.component.jsx'
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage/homepage.component.jsx";
+
+import "./App.css";
+
+const Hats = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
