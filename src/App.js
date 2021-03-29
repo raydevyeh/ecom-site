ad887106422f0aab8856bfec8c9f8c5db400e330
raydevyeh <raydevyeh@gmail.com>
2021-03-29 12:21:08 -0700

    added new sign-in component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from "./pages/homepage/homepage.component.jsx";
-import ShopPage from "./pages/shop/shop.component.jsx";
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx'
+import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInandSignUpPage} />
       </Switch>
     </div>
   );
